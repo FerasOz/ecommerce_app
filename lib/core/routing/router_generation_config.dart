f75d72271f803075eb9fb41dd193ccb6928c4b8c
FerasOz <ferasosama99@gmail.com>
2026-07-30 10:38:04 +0300

    - GoRouter added to the project

--- lib/core/routing/router_generation_config.dart
+++ lib/core/routing/router_generation_config.dart
@@ -1,1 +1,28 @@
-class RouterGenerationConfig {}
+import 'package:ecommerce_app/core/routing/app_routes.dart';
+import 'package:ecommerce_app/features/auth/login_screen.dart';
+import 'package:ecommerce_app/features/auth/register_screen.dart';
+import 'package:ecommerce_app/features/main/main_screen.dart';
+import 'package:go_router/go_router.dart';
+
+class RouterGenerationConfig {
+  static GoRouter goRouter = GoRouter(
+    initialLocation: AppRoutes.loginScreen,
+    routes: [
+      GoRoute(
+        name: AppRoutes.loginScreen,
+        path: AppRoutes.loginScreen,
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        name: AppRoutes.registerScreen,
+        path: AppRoutes.registerScreen,
+        builder: (context, state) => const RegisterScreen(),
+      ),
+      GoRoute(
+        name: AppRoutes.mainScreen,
+        path: AppRoutes.mainScreen,
+        builder: (context, state) => const MainScreen(),
+      ),
+    ],
+  );
+}
